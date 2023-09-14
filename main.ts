60e8765a15945399b4397f824f118f9bc060a1a1
Ali Rafieefar <aliraf62@gmail.com>
2023-09-14 17:53:48 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
 radio.onReceivedString(function (receivedString) {
     currentStrength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
-    if (currentStrength > calibrationValue + 5) {
+    // Compute moving average
+    total += 0 - readings[readIndex]
+    readings[readIndex] = currentStrength
+    total += readings[readIndex]
+    readIndex += 1
+    if (readIndex >= NUM_READINGS) {
+        readIndex = 0
+    }
+    average = total / NUM_READINGS
+    movingAverageStrength = average
+    if (movingAverageStrength > SOME_THRESHOLD) {
         isCloseEnough = true
     } else {
         isCloseEnough = false
     }
     if (receivedString == "ConfirmM2") {
         confirmed = true
+    } else if (receivedString == "Reset") {
+        confirmed = false
+        isCloseEnough = false
+        basic.clearScreen()
     } else if (isCloseEnough) {
-        // Introducing a slight delay so M2 and M3 won't interfere in giving win signal to M1
-        basic.pause(Math.randomRange(0, 1000))
         radio.sendString("M2Found")
     } else {
         radio.sendString("M2Searching")
@@ -17,9 +29,23 @@ radio.onReceivedString(function (receivedString) {
 })
 let confirmed = false
 let isCloseEnough = false
+let movingAverageStrength = 0
+let average = 0
+let readIndex = 0
+let total = 0
 let currentStrength = 0
+let SOME_THRESHOLD = 0
+let readings: number[] = []
+let NUM_READINGS = 0
 // Calibration Phase
 let calibrationValue = 0
+// for smoothing out readings
+// number of readings to average
+NUM_READINGS = 10
+for (let index = 0; index < NUM_READINGS; index++) {
+    readings.push(0)
+}
+let BUFFER = 10
 radio.setGroup(1)
 for (let index = 0; index < 5; index++) {
     radio.sendString("Calibrate")
@@ -27,8 +53,11 @@ for (let index = 0; index < 5; index++) {
     basic.pause(100)
 }
 calibrationValue /= 5
+SOME_THRESHOLD = calibrationValue + BUFFER
 basic.forever(function () {
     if (confirmed) {
+        basic.showIcon(IconNames.Happy)
+    } else if (isCloseEnough) {
         basic.showIcon(IconNames.Yes)
     } else {
         basic.showIcon(IconNames.No)
